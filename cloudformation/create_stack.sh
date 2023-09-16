1079ab8c5679853590df5fefe78d029ce3aba94c
Cong Dinh <congdinh2012@hotmail.com>
2023-09-16 22:51:44 +0700

    Update CloudFormation bash

--- cloudformation/create_stack.sh
+++ cloudformation/create_stack.sh
@@ -1,16 +1,9 @@
 ACTION=$1
 REGION="us-east-1"
+STACK="$2-stack"
+TEMPLATE="$2/$2.yml"
+PARAMS="$2/$2-parameters.json"
 
-# Define a list of items
-# my_list=("s3" "iam" "network" "bastion" "udagram")
-my_list=("udagram")
 
-# Loop through the list
-for item in "${my_list[@]}"; do
-    STACK="${item}-stack"
-    TEMPLATE="${item}/${item}.yml"
-    PARAMS="${item}/${item}-parameters.json"
-    echo "$ACTION stack $STACK from template $TEMPLATE with parameters $PARAMS:"
-    source ./stack_manager.sh $ACTION $STACK $TEMPLATE $PARAMS $REGION
-    sleep 20
-done
+echo "$ACTION stack $STACK from template $TEMPLATE with parameters $PARAMS:"
+source ./stack_manager.sh $ACTION $STACK $TEMPLATE $PARAMS $REGION
